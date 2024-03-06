2d63e7c490dafb37d273fc0fb77edeafc59ce2c2
Charles Bassett <chasb96@gmail.com>
2024-03-06 12:49:07 -0600

    Added host to cluster

--- host/src/lib.rs
+++ host/src/lib.rs
@@ -1,4 +1,5 @@
 mod routes;
 mod health;
+mod logging;
 
 pub use routes::routes;
--- host/src/main.rs
+++ host/src/main.rs
@@ -1,4 +1,5 @@
 use host::*;
+use log::info;
 
 use std::error::Error;
 use tokio::net::TcpListener;
@@ -6,8 +7,12 @@ use axum::serve;
 
 #[tokio::main]
 async fn main()  -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    info!("Binding to 0.0.0.0:80");
     let listener = TcpListener::bind("0.0.0.0:80").await?;
 
+    info!("Serving traffic");
     serve(listener, routes()).await?;
 
     Ok(())
