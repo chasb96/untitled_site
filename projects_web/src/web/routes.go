afdf930c3e1f381f1dd15636d7ff39996e70cfd5
Charles Bassett <chasb96@gmail.com>
2024-03-12 19:00:35 -0500

    Fix naming

--- projects_web/src/web/routes.go
+++ projects_web/src/web/routes.go
@@ -7,7 +7,7 @@ func (server *Server) Routes() *mux.Router {
 
 	server.Logger.Info("Registering routes")
 	router.HandleFunc("/health", server.health).Methods("GET")
-	router.HandleFunc("/user/{user_id}", loggerMiddleware(server.Logger, server.listByUser)).Methods("GET")
+	router.HandleFunc("/users/{user_id}", loggerMiddleware(server.Logger, server.listByUser)).Methods("GET")
 	router.HandleFunc("/{id}", loggerMiddleware(server.Logger, server.findProject)).Methods("GET")
 	router.HandleFunc("/", loggerMiddleware(server.Logger, authMiddleware(server.Logger, server.createProject))).Methods("POST")
 
