4ef17ce9fa29563031b848287157461320abc4ec
Charles Bassett <chasb96@gmail.com>
2024-04-24 19:22:53 -0500

    Fix deployment issue

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,6 +1,6 @@
 use api::{self, create_api_router};
 
-use std::{env, error::Error};
+use std::error::Error;
 use axum::serve;
 use log::info;
 use tokio::net::TcpListener;
@@ -9,12 +9,8 @@ use tokio::net::TcpListener;
 async fn main()  -> Result<(), Box<dyn Error>> {
     env_logger::init();
 
-    let port = env::var("API_PORT").unwrap_or("80".to_owned());
-
-    let socket_addr = format!("0.0.0.0:{}", port);
-
-    info!("Binding to {}", socket_addr);
-    let listener = TcpListener::bind(socket_addr).await?;
+    info!("Binding to 0.0.0.0:80");
+    let listener = TcpListener::bind("0.0.0.0:80").await?;
 
     info!("Serving traffic");
     serve(listener, create_api_router()).await?;
